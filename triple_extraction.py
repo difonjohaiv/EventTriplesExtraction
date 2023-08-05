aa2ac20a86dcaba1bc448d08ae8d26ae754a20a5
snailshaw <snailshaw@126.com>
2023-08-05 19:57:35 +0800

    0

--- triple_extraction.py
+++ triple_extraction.py
@@ -6,24 +6,38 @@
 from sentence_parser import *
 import re
 
+
 class TripleExtractor:
     def __init__(self):
         self.parser = LtpParser()
 
     '''文章分句处理, 切分长句，冒号，分号，感叹号等做切分标识'''
+
     def split_sents(self, content):
-        return [sentence for sentence in re.split(r'[？?！!。；;：:\n\r]', content) if sentence]
+        return [
+            sentence for sentence in re.split(r'[？?！!。；;：:\n\r]', content)
+            if sentence
+        ]
 
     '''利用语义角色标注,直接获取主谓宾三元组,基于A0,A1,A2'''
+
     def ruler1(self, words, postags, roles_dict, role_index):
         v = words[role_index]
         role_info = roles_dict[role_index]
         if 'A0' in role_info.keys() and 'A1' in role_info.keys():
-            s = ''.join([words[word_index] for word_index in range(role_info['A0'][1], role_info['A0'][2]+1) if
-                         postags[word_index][0] not in ['w', 'u', 'x'] and words[word_index]])
-            o = ''.join([words[word_index] for word_index in range(role_info['A1'][1], role_info['A1'][2]+1) if
-                         postags[word_index][0] not in ['w', 'u', 'x'] and words[word_index]])
-            if s  and o:
+            s = ''.join([
+                words[word_index] for word_index in range(
+                    role_info['A0'][1], role_info['A0'][2] + 1)
+                if postags[word_index][0] not in ['w', 'u', 'x']
+                and words[word_index]
+            ])
+            o = ''.join([
+                words[word_index] for word_index in range(
+                    role_info['A1'][1], role_info['A1'][2] + 1)
+                if postags[word_index][0] not in ['w', 'u', 'x']
+                and words[word_index]
+            ])
+            if s and o:
                 return '1', [s, v, o]
         # elif 'A0' in role_info:
         #     s = ''.join([words[word_index] for word_index in range(role_info['A0'][1], role_info['A0'][2] + 1) if
@@ -37,6 +51,7 @@ class TripleExtractor:
         return '4', []
 
     '''三元组抽取主函数'''
+
     def ruler2(self, words, postags, child_dict_list, arcs, roles_dict):
         svos = []
         for index in range(len(postags)):
@@ -51,13 +66,15 @@ class TripleExtractor:
                 # 如果语义角色标记为空，则使用依存句法进行抽取
                 # if postags[index] == 'v':
                 if postags[index]:
-                # 抽取以谓词为中心的事实三元组
+                    # 抽取以谓词为中心的事实三元组
                     child_dict = child_dict_list[index]
                     # 主谓宾
                     if 'SBV' in child_dict and 'VOB' in child_dict:
                         r = words[index]
-                        e1 = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0])
-                        e2 = self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
+                        e1 = self.complete_e(words, postags, child_dict_list,
+                                             child_dict['SBV'][0])
+                        e2 = self.complete_e(words, postags, child_dict_list,
+                                             child_dict['VOB'][0])
                         svos.append([e1, r, e2])
 
                     # 定语后置，动宾关系
@@ -65,9 +82,12 @@ class TripleExtractor:
                     head = arcs[index][2]
                     if relation == 'ATT':
                         if 'VOB' in child_dict:
-                            e1 = self.complete_e(words, postags, child_dict_list, head - 1)
+                            e1 = self.complete_e(words, postags,
+                                                 child_dict_list, head - 1)
                             r = words[index]
-                            e2 = self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
+                            e2 = self.complete_e(words, postags,
+                                                 child_dict_list,
+                                                 child_dict['VOB'][0])
                             temp_string = r + e2
                             if temp_string == e1[:len(temp_string)]:
                                 e1 = e1[len(temp_string):]
@@ -75,43 +95,55 @@ class TripleExtractor:
                                 svos.append([e1, r, e2])
                     # 含有介宾关系的主谓动补关系
                     if 'SBV' in child_dict and 'CMP' in child_dict:
-                        e1 = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0])
+                        e1 = self.complete_e(words, postags, child_dict_list,
+                                             child_dict['SBV'][0])
                         cmp_index = child_dict['CMP'][0]
                         r = words[index] + words[cmp_index]
                         if 'POB' in child_dict_list[cmp_index]:
-                            e2 = self.complete_e(words, postags, child_dict_list, child_dict_list[cmp_index]['POB'][0])
+                            e2 = self.complete_e(
+                                words, postags, child_dict_list,
+                                child_dict_list[cmp_index]['POB'][0])
                             svos.append([e1, r, e2])
         return svos
 
     '''对找出的主语或者宾语进行扩展'''
+
     def complete_e(self, words, postags, child_dict_list, word_index):
         child_dict = child_dict_list[word_index]
         prefix = ''
         if 'ATT' in child_dict:
             for i in range(len(child_dict['ATT'])):
-                prefix += self.complete_e(words, postags, child_dict_list, child_dict['ATT'][i])
+                prefix += self.complete_e(words, postags, child_dict_list,
+                                          child_dict['ATT'][i])
         postfix = ''
         if postags[word_index] == 'v':
             if 'VOB' in child_dict:
-                postfix += self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
+                postfix += self.complete_e(words, postags, child_dict_list,
+                                           child_dict['VOB'][0])
             if 'SBV' in child_dict:
-                prefix = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0]) + prefix
+                prefix = self.complete_e(words, postags, child_dict_list,
+                                         child_dict['SBV'][0]) + prefix
 
         return prefix + words[word_index] + postfix
 
     '''程序主控函数'''
+
     def triples_main(self, content):
         sentences = self.split_sents(content)
         svos = []
         for sentence in sentences:
-            words, postags, child_dict_list, roles_dict, arcs = self.parser.parser_main(sentence)
-            svo = self.ruler2(words, postags, child_dict_list, arcs, roles_dict)
+            words, postags, child_dict_list, roles_dict, arcs = self.parser.parser_main(
+                sentence)
+            svo = self.ruler2(words, postags, child_dict_list, arcs,
+                              roles_dict)
             svos += svo
 
         return svos
 
 
 '''测试'''
+
+
 def test():
     content1 = """环境很好，位置独立性很强，比较安静很切合店名，半闲居，偷得半日闲。点了比较经典的菜品，味道果然不错！烤乳鸽，超级赞赞赞，脆皮焦香，肉质细嫩，超好吃。艇仔粥料很足，香葱自己添加，很贴心。金钱肚味道不错，不过没有在广州吃的烂，牙口不好的慎点。凤爪很火候很好，推荐。最惊艳的是长寿菜，菜料十足，很新鲜，清淡又不乏味道，而且没有添加调料的味道，搭配的非常不错！"""
     content2 = """近日，一条男子高铁吃泡面被女乘客怒怼的视频引发热议。女子情绪激动，言辞激烈，大声斥责该乘客，称高铁上有规定不能吃泡面，质问其“有公德心吗”“没素质”。视频曝光后，该女子回应称，因自己的孩子对泡面过敏，曾跟这名男子沟通过，但对方执意不听，她才发泄不满，并称男子拍视频上传已侵犯了她的隐私权和名誉权，将采取法律手段。12306客服人员表示，高铁、动车上一般不卖泡面，但没有规定高铁、动车上不能吃泡面。
@@ -131,7 +163,8 @@ def test():
     content5 = ''' 以色列国防军20日对加沙地带实施轰炸，造成3名巴勒斯坦武装人员死亡。此外，巴勒斯坦人与以色列士兵当天在加沙地带与以交界地区发生冲突，一名巴勒斯坦人被打死。当天的冲突还造成210名巴勒斯坦人受伤。
     当天，数千名巴勒斯坦人在加沙地带边境地区继续“回归大游行”抗议活动。部分示威者燃烧轮胎，并向以军投掷石块、燃烧瓶等，驻守边境的以军士兵向示威人群发射催泪瓦斯并开枪射击。'''
     extractor = TripleExtractor()
-    svos = extractor.triples_main(content2)
+    svos = extractor.triples_main(content=content5)
     print('svos', svos)
+
 
 test()
