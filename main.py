7c390e3ed358029b831007058b74d95b40748f81
snailshaw <snailshaw@126.com>
2023-08-05 22:50:55 +0800

    0

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def start_extracting(i, extractor):
 
 if __name__ == '__main__':
     extractor = TripleExtractor()
-    content5 = ''' 以色列国防军20日对加沙地带实施轰炸，造成3名巴勒斯坦武装人员死亡。此外，巴勒斯坦人与以色列士兵当天在加沙地带与以交界地区发生冲突，一名巴勒斯坦人被打死。当天的冲突还造成210名巴勒斯坦人受伤。
-    当天，数千名巴勒斯坦人在加沙地带边境地区继续“回归大游行”抗议活动。部分示威者燃烧轮胎，并向以军投掷石块、燃烧瓶等，驻守边境的以军士兵向示威人群发射催泪瓦斯并开枪射击。'''
-    svos = extractor.triples_main(content=content5)
-    print(svos)
+    index_list = [1, 2, 3, 4, 5, 6, 7]
+    for item in index_list:
+        start_extracting(i=item, extractor=extractor)
